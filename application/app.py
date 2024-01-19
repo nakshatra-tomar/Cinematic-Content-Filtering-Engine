9d81039ec33676e7efa6562a1f18e221004bdce2
Nakshatra Tomar <tomarnakshatra@gmail.com>
2024-01-19 23:04:59 +0530

    serialization_01191

--- application/app.py
+++ application/app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pickle
+import pandas
+
+
 
 st.title('Cinematic Content Filtering Engine')
 
-st.selectbox('What movie would you like suggestions for?', ('Email', 'Home phone', 'Mobile phone'))
+movies_list = pickle.load(open('movies.pkl','rb'))
+
+movies_list = movies_list['titles'].values
+
+st.selectbox(
+    'What movie would you like suggestions for?',
+    ('Email', 'Home phone', 'Mobile phone'))
 

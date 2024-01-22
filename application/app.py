aeff8876f717c5310b104f3088f3b6e12a07473f
Nakshatra Tomar <tomarnakshatra@gmail.com>
2024-01-22 23:36:13 +0530

    app final

--- application/app.py
+++ application/app.py
@@ -3,6 +3,8 @@ import pickle
 import pandas
 import requests
 
+
+
 def recom(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
@@ -12,15 +14,15 @@ def recom(movie):
     recom_movies = []
     recom_poster = []
     for i in movies_list:
-        movie_id = i[0]
+        movie_id = movies.iloc[i[0]].movie_id
 
         recom_movies.append(movies.iloc[i[0]]['title'])
 
-        recom_poster.append(poster(i[0]))
-    return recom_movies, recom poster
+        recom_poster.append(poster(movie_id))
+    return recom_movies, recom_poster
 
 def poster(movie_id):
-    response = requests.get( https://api.themoviedb.org/3/movie/{}.format(movie_id))
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=627a9e020bf5b7690422ef0f3171f8ce".format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
@@ -41,5 +43,24 @@ selected_movie = st.selectbox(
 if st.button('Recommend me something similar'):
     recommendations,posters = recom(selected_movie)
 
-    for i in recommendations:
-        st.write(i)
+    col1, col2, col3, col4, col5  = st.columns(5)
+
+    with col1:
+        st.text(recommendations[0])
+        st.image(posters[0])
+
+    with col2:
+        st.text(recommendations[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(recommendations[2])
+        st.image(posters[2])
+
+    with col4:
+        st.text(recommendations[3])
+        st.image(posters[3])
+
+    with col5:
+        st.text(recommendations[4])
+        st.image(posters[4])
